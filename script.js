56d290b449a6cf5ad69e0377597a792217f8da0c
Julia <juliadufresne@gmail.com>
2020-02-14 15:24:13 -0500

    massaged ajax call

---
+++ script.js
@@ -0,0 +1,51 @@
+const app = {};
+
+// WORKING URL THAT PULLS ENTIRE API BY CITY (TORONTO)
+// https://app.ticketmaster.com/discovery/v2/events.json?apikey=bTDdH2M6LG5xlXjLYgM6g2xJnQJgtML1&city=toronto
+
+app.ajaxCall = function(){
+    $.ajax({
+        url: `https://app.ticketmaster.com/discovery/v2/events.json?`,
+        type: `GET`,
+        datatype: `json`,
+        async: true,
+        data: {
+            apikey: `bTDdH2M6LG5xlXjLYgM6g2xJnQJgtML1`,
+            city: `toronto`,
+        }
+    }).then(function(result) {
+        // console.log(result)
+
+        // Genre
+        console.log(result._embedded.events[0].classifications[0].segment.name)
+
+        // Type of genre
+        console.log(result._embedded.events[0].name)
+
+        // Venue
+        console.log(result._embedded.events[0]._embedded.venues[0].name)
+
+        // Pulls date
+        console.log(result._embedded.events[0].dates.start.localDate);
+
+        // Pulls time
+        console.log(result._embedded.events[0].dates.start.localTime);
+    })
+}
+
+
+app.date = $('.date');
+console.log(app.date.val())
+
+app.init = function(){
+    app.ajaxCall();
+}
+
+$(function() {
+    app.init();
+})
+
+
+
+
+
