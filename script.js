fd7f4bbbdcfb0a87cf2a0bd327611b858f1061c4
Julia <juliadufresne@gmail.com>
2020-02-14 16:28:22 -0500

    added data in ajax call

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const app = {};
 // WORKING URL THAT PULLS ENTIRE API BY CITY (TORONTO)
 // https://app.ticketmaster.com/discovery/v2/events.json?apikey=bTDdH2M6LG5xlXjLYgM6g2xJnQJgtML1&city=toronto
 
+
+// Storing date from HTML
+app.date = $('.date').val();
+console.log(app.date);
+
+app.time = `T00:00:00Z`
+
+app.dateTime = 
+
 app.ajaxCall = function(){
     $.ajax({
         url: `https://app.ticketmaster.com/discovery/v2/events.json?`,
@@ -12,12 +21,10 @@ app.ajaxCall = function(){
         data: {
             apikey: `bTDdH2M6LG5xlXjLYgM6g2xJnQJgtML1`,
             city: `toronto`,
-            startDateTime: `YYYY-MM-DDTHH:MM:SSZ`,
-            endDateTime: `YYYY-MM-DDTHH:MM:SSZ`,
+            // startDateTime: `YYYY-MM-DDTHH:MM:SSZ`,
+            // endDateTime: `YYYY-MM-DDTHH:MM:SSZ`,
         }
     }).then(function(result) {
-        console.log(result)
-
         // Genre
         console.log(result._embedded.events[0].classifications[0].segment.name)
 
@@ -34,10 +41,6 @@ app.ajaxCall = function(){
         console.log(result._embedded.events[0].dates.start.localTime);
     })
 }
-
-
-app.date = $('.date');
-console.log(app.date.val())
 
 app.init = function(){
     app.ajaxCall();
