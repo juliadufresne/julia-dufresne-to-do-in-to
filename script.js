5551276adfa1e4b8caad4e654344a92aca9c04c5
Julia <juliadufresne@gmail.com>
2020-02-18 14:44:29 -0500

    no longer appending duplicate events

--- script.js
+++ script.js
@@ -102,13 +102,36 @@ app.ajaxCall = function(){
         })
 
         // Arts
-        arrayData.forEach(function(currentItem){
-            const genre = currentItem.classifications[0].segment.name;
-            const nameOfEvent = currentItem.name;
-            const venue = currentItem._embedded.venues[0].name;
-            const date = currentItem.dates.start.localDate;
-            const time = currentItem.dates.start.localTime;
-            if (genre === "Arts & Theatre") {
+        // arrayData.forEach(function(currentItem){
+        //     const genre = currentItem.classifications[0].segment.name;
+        //     const nameOfEvent = currentItem.name;
+        //     const venue = currentItem._embedded.venues[0].name;
+        //     const date = currentItem.dates.start.localDate;
+        //     const time = currentItem.dates.start.localTime;
+        //     if (genre === "Arts & Theatre") {
+        //         const htmlToAppend = `
+        //         <div>
+        //             <li>Genre: ${genre}</li>
+        //             <li>Name of event: ${nameOfEvent}</li>
+        //             <li>Name of venue: ${venue}</li>
+        //             <li>Date: ${date}</li>
+        //             <li>Time: ${time}</li>
+        //         </div>
+        //         `;
+        //         $('.arts').append(htmlToAppend);
+        //     }
+        // })
+
+        // Function to sort out duplicate events
+        function sortDuplicates() {
+            // New array that we will not add duplicate events to (no two events with the same name)
+            let noDuplicates = [];
+            arrayData.forEach(function(item){
+                const genre = item.classifications[0].segment.name;
+                const nameOfEvent = item.name;
+                const venue = item._embedded.venues[0].name;
+                const date = item.dates.start.localDate;
+                const time = item.dates.start.localTime;
                 const htmlToAppend = `
                 <div>
                     <li>Genre: ${genre}</li>
@@ -118,60 +141,36 @@ app.ajaxCall = function(){
                     <li>Time: ${time}</li>
                 </div>
                 `;
-                $('.arts').append(htmlToAppend);
-            }
-        })
-
-        // Function to sort out duplicate events
-        function sortDuplicates() {
-
-            // New array that we will not add duplicate events to (no two events with the same name)
-            let noDuplicates = [];
-
-            arrayData.forEach(function(item){
-
-                const genre = item.classifications[0].segment.name;
 
                 // We only care about "item" if its an arts event
                 if (genre === "Arts & Theatre") {
-                    
                     // If there's nothing in noDuplicates, that means that we can safely add our first event and it won't be a duplicate
                     if(noDuplicates.length == 0){
-                        
                         noDuplicates.push(item);
-                    
+                        htmlToAppend;
+                        $('.arts').append(htmlToAppend);
                     // Otherwise, we have to take a look at what we've added to noDuplicates already
                     } else {
-
                         // Boolean variable that tells us if the "copied" item matches the "item" we're considering adding 
                         let match = false;
-
                         noDuplicates.forEach(function(copied) {
-
                             // If the name and venue are the same, we treat this as the same event, and we don't add it to noDuplicates
                             if(item.name === copied.name && 
                                 item._embedded.venues[0].name === copied._embedded.venues[0].name) {
-
                                     match = true;
-
                             }
-
                         });
-
                         // If no event in noDuplicates matches item, that means we can add it.
                         if(!match) {
                             noDuplicates.push(item);
+                            htmlToAppend;
+                            $('.arts').append(htmlToAppend);
                         }
-
                     }
-                        
                 }
             })
-
             console.log(noDuplicates);
-
         }
-
         sortDuplicates();
     })
 }
