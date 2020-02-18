2f64c7175240c9c70a89bb2db1f70a129ec3d4ad
Julia <juliadufresne@gmail.com>
2020-02-18 18:25:11 -0500

    started css

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const app = {};
 
-// WORKING URL THAT PULLS ENTIRE API BY CITY (TORONTO)
-// https://app.ticketmaster.com/discovery/v2/events.json?apikey=bTDdH2M6LG5xlXjLYgM6g2xJnQJgtML1&city=toronto
-
-
 // Storing date from HTML
 app.date = $('.date').val();
-// app.submit = $('.submit');
 
-$(".submit").click(function(){
-
+// Submit button
+app.submitButton = $(".submit").click(function(){
     location.reload();
-
 });
 
 // PULLS LOCAL DATE TIME
@@ -23,9 +17,7 @@ app.localStartDateTime = `${app.startDateTime},${app.endDateTime}`
 
 // Function to sort out duplicate events
 app.sortSportsDuplicates = function(result) {
-
     let arrayData = result._embedded.events;
-
     // New array that we will not add duplicate events to (no two events with the same name)
     let noDuplicates = [];
     arrayData.forEach(function(item){
@@ -35,7 +27,6 @@ app.sortSportsDuplicates = function(result) {
         const date = item.dates.start.localDate;
         const time = item.dates.start.localTime;
         const image = item.images;
-
             image.forEach(function(i){
                 if (i.width === 205) {
                     return imageURL = i.url;
@@ -51,7 +42,6 @@ app.sortSportsDuplicates = function(result) {
             <li>Image: <img src="${imageURL}" alt=""></li>
         </div>
         `;
-
         // We only care about "item" if its an arts event
         if (genre === "Sports") {
             // If there's nothing in noDuplicates, that means that we can safely add our first event and it won't be a duplicate
@@ -82,77 +72,9 @@ app.sortSportsDuplicates = function(result) {
 }
 
 
-
-
-        // Function to sort out duplicate events
-        app.sortMusicDuplicates = function(result) {
-
-            let arrayData = result._embedded.events;
-
-            // New array that we will not add duplicate events to (no two events with the same name)
-            let noDuplicates = [];
-            arrayData.forEach(function(item){
-                const genre = item.classifications[0].segment.name;
-                const nameOfEvent = item.name;
-                const venue = item._embedded.venues[0].name;
-                const date = item.dates.start.localDate;
-                const time = item.dates.start.localTime;
-                const image = item.images;
-
-                    image.forEach(function(i){
-                        if (i.width === 205) {
-                            return imageURL = i.url;
-                        }
-                    })
-
-                const htmlToAppend = `
-                <div>
-                    <li>Genre: ${genre}</li>
-                    <li>Name of event: ${nameOfEvent}</li>
-                    <li>Name of venue: ${venue}</li>
-                    <li>Date: ${date}</li>
-                    <li>Time: ${time}</li>
-                    <li>Image: <img src="${imageURL}" alt=""></li>
-                </div>
-                `;
-
-                // We only care about "item" if its an arts event
-                if (genre === "Music") {
-                    // If there's nothing in noDuplicates, that means that we can safely add our first event and it won't be a duplicate
-                    if(noDuplicates.length == 0){
-                        noDuplicates.push(item);
-                        htmlToAppend;
-                        $('.music').append(htmlToAppend);
-                    // Otherwise, we have to take a look at what we've added to noDuplicates already
-                    } else {
-                        // Boolean variable that tells us if the "copied" item matches the "item" we're considering adding 
-                        let match = false;
-                        noDuplicates.forEach(function(copied) {
-                            // If the name and venue are the same, we treat this as the same event, and we don't add it to noDuplicates
-                            if(item.name === copied.name && 
-                                item._embedded.venues[0].name === copied._embedded.venues[0].name) {
-                                    match = true;
-                            }
-                        });
-                        // If no event in noDuplicates matches item, that means we can add it.
-                        if(!match) {
-                            noDuplicates.push(item);
-                            htmlToAppend;
-                            $('.music').append(htmlToAppend);
-                        }
-                    }
-                }
-            })
-        }
-
-
-
-
 // Function to sort out duplicate events
-app.sortArtDuplicates = function(result) {
-
+app.sortMusicDuplicates = function(result) {
     let arrayData = result._embedded.events;
-
     // New array that we will not add duplicate events to (no two events with the same name)
     let noDuplicates = [];
     arrayData.forEach(function(item){
@@ -162,13 +84,11 @@ app.sortArtDuplicates = function(result) {
         const date = item.dates.start.localDate;
         const time = item.dates.start.localTime;
         const image = item.images;
-
             image.forEach(function(i){
                 if (i.width === 205) {
                     return imageURL = i.url;
                 }
             })
-
         const htmlToAppend = `
         <div>
             <li>Genre: ${genre}</li>
@@ -179,7 +99,63 @@ app.sortArtDuplicates = function(result) {
             <li>Image: <img src="${imageURL}" alt=""></li>
         </div>
         `;
+        // We only care about "item" if its an arts event
+        if (genre === "Music") {
+            // If there's nothing in noDuplicates, that means that we can safely add our first event and it won't be a duplicate
+            if(noDuplicates.length == 0){
+                noDuplicates.push(item);
+                htmlToAppend;
+                $('.music').append(htmlToAppend);
+            // Otherwise, we have to take a look at what we've added to noDuplicates already
+            } else {
+                // Boolean variable that tells us if the "copied" item matches the "item" we're considering adding 
+                let match = false;
+                noDuplicates.forEach(function(copied) {
+                    // If the name and venue are the same, we treat this as the same event, and we don't add it to noDuplicates
+                    if(item.name === copied.name && 
+                        item._embedded.venues[0].name === copied._embedded.venues[0].name) {
+                            match = true;
+                    }
+                });
+                // If no event in noDuplicates matches item, that means we can add it.
+                if(!match) {
+                    noDuplicates.push(item);
+                    htmlToAppend;
+                    $('.music').append(htmlToAppend);
+                }
+            }
+        }
+    })
+}
 
+
+// Function to sort out duplicate events
+app.sortArtDuplicates = function(result) {
+    let arrayData = result._embedded.events;
+    // New array that we will not add duplicate events to (no two events with the same name)
+    let noDuplicates = [];
+    arrayData.forEach(function(item){
+        const genre = item.classifications[0].segment.name;
+        const nameOfEvent = item.name;
+        const venue = item._embedded.venues[0].name;
+        const date = item.dates.start.localDate;
+        const time = item.dates.start.localTime;
+        const image = item.images;
+            image.forEach(function(i){
+                if (i.width === 205) {
+                    return imageURL = i.url;
+                }
+            })
+        const htmlToAppend = `
+        <div>
+            <li>Genre: ${genre}</li>
+            <li>Name of event: ${nameOfEvent}</li>
+            <li>Name of venue: ${venue}</li>
+            <li>Date: ${date}</li>
+            <li>Time: ${time}</li>
+            <li>Image: <img src="${imageURL}" alt=""></li>
+        </div>
+        `;
         // We only care about "item" if its an arts event
         if (genre === "Arts & Theatre") {
             // If there's nothing in noDuplicates, that means that we can safely add our first event and it won't be a duplicate
