7d1109d8364637f887a0a6be089026e0da055b96
Julia <juliadufresne@gmail.com>
2020-02-17 20:41:37 -0500

    fixed date searching

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ const app = {};
 
 // Storing date from HTML
 app.date = $('.date').val();
-
-app.startTime = `T00:00:00Z`;
-app.endTime = `T23:59:59Z`;
-
+app.startTime = `T00:00:00`;
 app.startDateTime = `${app.date}${app.startTime}`;
+app.endTime = `T23:59:59`;
 app.endDateTime = `${app.date}${app.endTime}`;
+
+app.localStartDateTime = `${app.startDateTime},${app.endDateTime}`
+console.log(app.localStartDateTime);
+
+
+// app.startTime = `T00:00:00Z`;
+// app.endTime = `T23:59:59Z`;
+// app.startDateTime = `${app.date}${app.startTime}`;
+// app.endDateTime = `${app.date}${app.endTime}`;
+
 
 app.ajaxCall = function(){
     $.ajax({
@@ -22,8 +30,9 @@ app.ajaxCall = function(){
         data: {
             apikey: `bTDdH2M6LG5xlXjLYgM6g2xJnQJgtML1`,
             city: `toronto`,
-            startDateTime: app.startDateTime,
-            endDateTime: app.endDateTime,
+            // startDateTime: app.startDateTime,
+            // endDateTime: app.endDateTime,
+            localStartDateTime: app.localStartDateTime,
         }
     }).then(function(result) {
 
@@ -44,19 +53,18 @@ app.ajaxCall = function(){
         // Pulls time
         // console.log(result._embedded.events[0].dates.start.localTime);
 
-        let arrayData = result._embedded.events.slice(0, 10);
+        let arrayData = result._embedded.events;
 
-
-        const sports = arrayData.forEach(function(currentItem){
+        // Sports
+        arrayData.forEach(function(currentItem){
             const genre = currentItem.classifications[0].segment.name
             const nameOfEvent = currentItem.name;
             const venue = currentItem._embedded.venues[0].name;
             const date = currentItem.dates.start.localDate;
             const time = currentItem.dates.start.localTime;
             if (genre === "Sports") {
-                // PRINT FIRST 10 EVENTS
                     const htmlToAppend = `
-                    <div class="sports">
+                    <div>
                         <li>Genre: ${genre}</li>
                         <li>Name of event: ${nameOfEvent}</li>
                         <li>Name of venue: ${venue}</li>
@@ -68,7 +76,8 @@ app.ajaxCall = function(){
             }
         }) 
 
-        const music = arrayData.forEach(function(currentItem){
+        // Music
+        arrayData.forEach(function(currentItem){
             const genre = currentItem.classifications[0].segment.name;
             const nameOfEvent = currentItem.name;
             const venue = currentItem._embedded.venues[0].name;
@@ -76,7 +85,7 @@ app.ajaxCall = function(){
             const time = currentItem.dates.start.localTime;
             if (genre === "Music"){
                 const htmlToAppend = `
-                <div class="music">
+                <div>
                     <li>Genre: ${genre}</li>
                     <li>Name of event: ${nameOfEvent}</li>
                     <li>Name of venue: ${venue}</li>
@@ -89,7 +98,8 @@ app.ajaxCall = function(){
             }
         })
 
-        const arts = arrayData.forEach(function(currentItem){
+        // Arts
+        arrayData.forEach(function(currentItem){
             const genre = currentItem.classifications[0].segment.name;
             const nameOfEvent = currentItem.name;
             const venue = currentItem._embedded.venues[0].name;
@@ -97,7 +107,7 @@ app.ajaxCall = function(){
             const time = currentItem.dates.start.localTime;
             if (genre === "Arts & Theatre") {
                 const htmlToAppend = `
-                <div class="arts">
+                <div>
                     <li>Genre: ${genre}</li>
                     <li>Name of event: ${nameOfEvent}</li>
                     <li>Name of venue: ${venue}</li>
@@ -108,6 +118,58 @@ app.ajaxCall = function(){
                 $('.arts').append(htmlToAppend);
             }
         })
+
+        // Function to sort out duplicate events
+        function connorAttempt() {
+
+            // New array that we will not add duplicate events to (no two events with the same name)
+            let noDupes = [];
+
+            arrayData.forEach(function(item){
+
+                const genre = item.classifications[0].segment.name;
+
+                // We only care about "item" if its an arts event
+                if (genre === "Arts & Theatre") {
+                    
+                    // If there's nothing in noDupes, that means that we can safely add our first event and it won't be a duplicate
+                    if(noDupes.length == 0){
+                        
+                        noDupes.push(item);
+                    
+                    // Otherwise, we have to take a look at what we've added to noDupes already
+                    } else {
+
+                        // Boolean variable that tells us if the "copied" item matches the "item" we're considering adding 
+                        let match = false;
+
+                        noDupes.forEach(function(copied) {
+
+                            // If the name and venue are the same, we treat this as the same event, and we don't add it to noDupes
+                            if(item.name === copied.name && 
+                                item._embedded.venues[0].name === copied._embedded.venues[0].name) {
+
+                                    match = true;
+
+                            }
+
+                        });
+
+                        // If no event in noDupes matches item, that means we can add it.
+                        if(!match) {
+                            noDupes.push(item);
+                        }
+
+                    }
+                        
+                }
+            })
+
+            console.log(noDupes);
+
+        }
+
+        connorAttempt();
     })
 }
 
